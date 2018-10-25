7ae48c857e67840dc71c0263eea26fe0d8ac8d1f
Phillip DiPhillipo <diphillipophillip@gmail.com>
2018-10-25 16:41:44 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
+
+
 def oxford_comma(array)
+if array.length == 1 
+  array.join()
+  elsif array.length == 2 
+  array.join(' and ')
+  arr = array.pop
+  elsif array.length > 3
+  array.join(', ') + #{arr}
+  
+end
 
 end
