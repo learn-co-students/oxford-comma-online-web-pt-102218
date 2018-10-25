8cff1d0c9c55dbae3e91c40bac288be140c2b691
Phillip DiPhillipo <diphillipophillip@gmail.com>
2018-10-25 20:03:54 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,15 @@
 
 
 def oxford_comma(array)
+
 if array.length == 1 
-  array.join()
-  elsif array.length == 2 
-  array.join(' and ')
-  arr = array.pop
-  elsif array.length > 3
-  array.join(', ') + #{arr}
-  
+  array.pop 
+elsif array.length == 2
+array.join(' and ')
+elsif array.length >= 3
+new = array.pop
+array.push("and #{new}") 
+array.join(', ' )
+end
 end
 
-end
