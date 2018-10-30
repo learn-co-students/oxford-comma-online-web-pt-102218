9996364068b0f317a749c048ad2911cffbc69e0b
A. V <anysiavalko@gmail.com>
2018-10-30 22:11:16 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+ if array.size == 1
+  return array.join
+ elsif array.size == 2
+  return array.join(" and ")
+else
+  new_last_item = "and #{array.last}"
+  array.pop
+  array.push(new_last_item)
+  return array.join(", ")
+ end
 end
